47595c71ecc53289eff998f3ff77d38c13cde086
Sofia Gukelberger <sofiagukelb@gmail.com>
2026-01-23 17:44:43 -0500

    Set up tailwind, implement responsiveness in about, art, and writing pages

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about',
   imports: [
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './about.html',
   styleUrl: './about.css',
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-
 @Component({
   selector: 'app-home',
   imports: [
